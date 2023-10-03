b051668c0737633f6a011a9c9681a4b75c6ef25d
rocky114 <934313141@qq.com>
2023-10-03 20:48:00 +0800

    xml

--- test.cpp
+++ test.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 int main() {
-    int a;
+    std::string input = "tag1.tag2.tag3~name";
+    std::vector<std::string> tokens;
+    
+    std::istringstream iss(input);
+    std::string token;
 
-    std::cin >> a;
-
-    std::cout << a << std::endl;
-
+    size_t pos {};
+    size_t last_pos {};
+    while ((pos = input.find_first_of(".~", last_pos)) != std::string::npos) {
+        tokens.push_back(input.substr(last_pos, pos - last_pos));
+        last_pos = pos + 1;
+    }
+    tokens.push_back(input.substr(last_pos));
+    
+    // 打印分隔后的结果
+    for (const std::string& t : tokens) {
+        std::cout << t << std::endl;
+    }
+    
     return 0;
 }
--- xml.cpp
+++ xml.cpp
@@ -1,54 +1,213 @@
 #include <iostream>
 #include <sstream>
 #include <map>
+#include <vector>
 
 
 struct element {
 	std::string tag {};
 	std::map<std::string, std::string> attributes {};
 	std::map<std::string, element> children {};
+	bool is_closing_tag {false};
 };
 
-void extract(std::map<std::string, struct element>& root, const std::string& line) 
+struct element append_child(struct element node, struct element current);
+
+void extract(struct element& root, const std::string line) 
 {
 	std::string::size_type start = line.find('<');
 	std::string::size_type end = line.find('>');
-
-	std::cout << start << end << std::endl;
-
 	std::string tagContent = line.substr(start + 1, end - start - 1);
 
-	std::cout << tagContent << std::endl;
+	struct element current {};
 
-	return ;
+	if (tagContent[0] == '/') {
+		tagContent = tagContent.substr(1);
+	}
+
+	std::stringstream ss(tagContent);
+	std::string word;
+
+	std::string key {};
+	std::string val {};
+	bool is_equal_operator {false};
+
+	for (int i = 0; ss >> word; i++) {
+		if (i == 0) {
+			current.tag = word;
+		}
+
+		if (word == "=") {
+			is_equal_operator = true;
+
+			continue;
+		}
+
+		if (is_equal_operator) {
+			val = word.substr(1, word.size() - 2);
+		} else {
+			key = word;
+
+			continue;
+		}
+
+		is_equal_operator = false;
+		current.attributes[key] = val;
+	}
+
+	if (root.tag == "") {
+		root.tag = current.tag;
+		root.attributes = current.attributes;
+
+		return;
+	}
+
+	if (root.tag == current.tag) {
+		root.is_closing_tag = true;
+		return;
+	}
+
+	root = append_child(root, current);
+}
+
+struct element append_child(struct element node, struct element current) 
+{
+	for (auto& pair : node.children) {		
+		if (!pair.second.is_closing_tag) {
+			pair.second = append_child(pair.second, current);
+
+			return node;
+		}
+	}
+	
+	if (node.tag == current.tag) {
+		node.is_closing_tag = true;
+		return node;
+	}
+
+	node.children[current.tag] = current;
+
+	return node;
+}
+
+void echo (struct element current) 
+{
+	std::cout << "echo tag: " << current.tag << " attribute: ";
+	for (auto& pair : current.attributes) {
+		std::cout << pair.first << " = " << pair.second;
+	}
+	std::cout << std::endl;
+
+	for (auto& pair : current.children) {
+		echo(pair.second);
+	}
+}
+
+std::string parse(struct element root, std::string line) {
+	std::vector<std::string> result {};
+
+	size_t pos {};
+    size_t last_pos {};
+    while ((pos = line.find_first_of(".~", last_pos)) != std::string::npos) {
+        result.push_back(line.substr(last_pos, pos - last_pos));
+        last_pos = pos + 1;
+    }
+    result.push_back(line.substr(last_pos));
+
+	auto length = result.size() - 1;
+
+	std::string attr = result[length];
+
+	struct element current_node = root;
+
+	int cout {0};
+	for (int i = 1; i < length; i++) {
+		for (auto pair : current_node.children) {
+			if (pair.first == result[i]) {
+				cout++;
+				current_node = pair.second;
+				break;
+			}
+		}
+	}
+
+	if (cout + 1 < length) {
+		return "Not Found!";
+	}
+
+	auto it = current_node.attributes.find(attr);
+	if (it != current_node.attributes.end()) {
+		return current_node.attributes[attr];
+	}
+
+	return "Not Found!";
 }
 
 int main() 
-{	
-	std::map<std::string, struct element> root {};
-
-	std::string str {"<tag1 value = \"HelloWorld\">"};
-	extract(root, str);
-
-	return 0;
-	
-	/* 
-	int number {}, query {};
+{		
+	int number {6}, query {4};
 
 	std::cin >> number >> query;
 
 	std::cin.ignore();
 
-	// std::map<std::string, struct element> root {};
+	struct element root {};
 
 	for (int i = 0; i < number; i++) {
 		std::string line {};
 		std::getline(std::cin, line);
 
-		extract(root, line);
+		// switch (i)
+		// {
+		// case 0:
+		// 	line = "<a>";
+		// 	break;
+		// case 1:
+		// 	line = "<b name = \"tag_one\">";
+		// 	break;
+		// case 2:
+		// 	line = "<c name = \"tag_two\" value = \"val_907\">";
+		// 	break;
+		// case 3:
+		// 	line = "</c>";
+		// 	break;
+		// case 4:
+		// 	line = "</b>";
+		// 	break;
+		// case 5:
+		// 	line = "</a>";
+		// 	break;
+		// default:
+		// 	break;
+		// }
 
-		std::cout << line << std::endl;
+		extract(root, line);
 	}
 
-	return 0;*/
+	for (int i = 0; i < query; i++) {
+		std::string line {};
+		std::getline(std::cin, line);
+
+		// switch (i)
+		// {
+		// case 0:
+		// 	line = "a.b~name";
+		// 	break;
+		// case 1:
+		// 	line = "a.b.c~value";
+		// 	break;
+		// case 2:
+		// 	line = "a.b.c~src";
+		// 	break;
+		// case 3:
+		// 	line = "a.b.c.d~name";
+		// 	break;
+		// default:
+		// 	break;
+		// }
+
+		std::cout << parse(root, line) << std::endl;
+	}
+
+	return 0;
 }
